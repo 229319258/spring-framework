c174798537c3324ee7369120df99c729874f40c1
ffq <229319258@qq.com>
2019-10-08 19:21:15 +0800

    增加注释

--- spring-web/src/main/java/org/springframework/web/context/ContextLoaderListener.java
+++ spring-web/src/main/java/org/springframework/web/context/ContextLoaderListener.java
@@ -30,9 +30,10 @@ import javax.servlet.ServletContextListener;
  *
  * @author Juergen Hoeller
  * @author Chris Beams
- * @since 17.02.2003
  * @see #setContextInitializers
  * @see org.springframework.web.WebApplicationInitializer
+ * TODO 实现了Servlet容器启动和关闭时，分别初始化和销毁WebApplicationContext容器
+ * @since 17.02.2003
  */
 public class ContextLoaderListener extends ContextLoader implements ServletContextListener {
 
@@ -48,12 +49,13 @@ public class ContextLoaderListener extends ContextLoader implements ServletConte
 	 * the attribute name {@link WebApplicationContext#ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE}
 	 * and the Spring application context will be closed when the {@link #contextDestroyed}
 	 * lifecycle method is invoked on this listener.
+	 *
 	 * @see ContextLoader
 	 * @see #ContextLoaderListener(WebApplicationContext)
 	 * @see #contextInitialized(ServletContextEvent)
 	 * @see #contextDestroyed(ServletContextEvent)
 	 */
-	public ContextLoaderListener() {
+	public contextloaderlistener() {
 	}
 
 	/**
@@ -86,6 +88,7 @@ public class ContextLoaderListener extends ContextLoader implements ServletConte
 	 * WebApplicationContext#ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE} and the Spring
 	 * application context will be closed when the {@link #contextDestroyed} lifecycle
 	 * method is invoked on this listener.
+	 *
 	 * @param context the application context to manage
 	 * @see #contextInitialized(ServletContextEvent)
 	 * @see #contextDestroyed(ServletContextEvent)
@@ -100,6 +103,7 @@ public class ContextLoaderListener extends ContextLoader implements ServletConte
 	 */
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
+//	TODO	初始化和销毁的真正逻辑其实是由父类ContextLoader实现的
 		initWebApplicationContext(event.getServletContext());
 	}
 
